c8e8bbd476051f5f5c81b2baa9df47bbdf334cca
Wandrey Warley <wandrey09@gmail.com>
2022-02-10 13:55:08 -0300

    att

--- js/script.js
+++ js/script.js
@@ -2,15 +2,8 @@ $(window).on("load", function(){
   console.log('carregou');
   loadEnd();
 });
-// setTimeout(() => {
-//   $('#website').css('display', 'block');
-//   $('#loading').css('display', 'none');
-// }, 1000);
 
 function loadEnd() {
   console.log('load end')
   $('#loading').slideUp();
 }
-
-
-console.log('script')
